94336d830962f7f2291b38ee4dd0a2a1e403bde7
Joao Victor <garcia_sgjv@hotmail.com>
2022-12-20 02:32:20 -0800

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -21,5 +21,5 @@ reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes(hospedes)}");
 Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
